b0514244a17d71424c2b322be3e11ba1bcdf2a9b
Kwabang01 <Kwabang01@gmail.com>
2020-01-23 19:58:55 +0100

    commit

--- src/app/salesreport/salesreport.component.ts
+++ src/app/salesreport/salesreport.component.ts
@@ -18,6 +18,8 @@ export class SalesreportComponent implements OnInit {
   result: any;
   loans: any;
   customers: any;
+  reportdata: any;
+  reportdataindividal: any[];
   
   constructor(private loadingBar: LoadingBarService,
     private service : AppServiceService,
@@ -36,6 +38,68 @@ export class SalesreportComponent implements OnInit {
     console.log(customers)
     this.customers = customers
     document.getElementById("modaltransaction").click()
+  }
+
+
+  exportResult(customers, marketer){
+
+    let marketers = "";
+
+    if(marketer =='Ugbobuaku' ){
+      marketer = "Ugbobuaku Mmesoma";
+    }
+
+    if(marketer =='Eke' ){
+      marketer = "Eke Emmanuella";
+    }
+
+    if(marketer =='Nnamso' ){
+      marketer = "Nnamso violet";
+    }
+
+
+    if(marketer =='blessing' ){
+      marketer = "Blessing Ebute";
+    }
+
+    if(marketer =='Atewe' ){
+      marketer = "Atewe Evans";
+    }
+
+    if(marketer =='osas' ){
+      marketer = "Aganmwonyi Osas";
+    }
+
+    if(marketer =='okitika' ){
+      marketer = "Okitika Mercy";
+    }
+
+    if(marketer =='akinbode' ){
+      marketer = "Akinbode Kofoworola";
+    }
+
+    if(marketer =='adedoyinojo' ){
+      marketer = "Adedoyin Ojo";
+    }
+
+    if(marketer =='tugboboayotunde' ){
+      marketer = "Tugbobo Ayotunde";
+    }
+   this.reportdataindividal = []
+    for (let index = 0; index < customers.length; ++index) {
+      let json = {
+        "S/No" : index + 1,
+        "Customer Name" : customers[index].firstname + " " +  customers[index].lastname,
+        "Amount" :  customers[index].loan_amount,
+        "Loan Id" :  customers[index].loanid,
+        "Date of Disbursement" :  customers[index].date_of_disbursement,
+      }
+      this.reportdataindividal[index] = json
+    }
+
+    this.service.exportAsExcelFile(this.reportdataindividal, 'Individual Report  for '+marketer);
+    
+   
   }
 
   getReport(){
@@ -82,6 +146,70 @@ export class SalesreportComponent implements OnInit {
           this.toastr.error("Network Error, please try again", '');
         }
     );
+  }
+
+  exportReport(){
+    this.reportdata = []
+    for (let index = 0; index < this.loans.length; ++index) {
+
+      let marketer = "";
+      if(this.loans[index].marketer =='Ugbobuaku' ){
+        marketer = "Ugbobuaku Mmesoma";
+      }
+
+      if(this.loans[index].marketer =='Eke' ){
+        marketer = "Eke Emmanuella";
+      }
+
+      if(this.loans[index].marketer =='Nnamso' ){
+        marketer = "Nnamso violet";
+      }
+
+
+      if(this.loans[index].marketer =='blessing' ){
+        marketer = "Blessing Ebute";
+      }
+
+      if(this.loans[index].marketer =='Atewe' ){
+        marketer = "Atewe Evans";
+      }
+
+      if(this.loans[index].marketer =='osas' ){
+        marketer = "Aganmwonyi Osas";
+      }
+
+      if(this.loans[index].marketer =='okitika' ){
+        marketer = "Okitika Mercy";
+      }
+
+      if(this.loans[index].marketer =='akinbode' ){
+        marketer = "Akinbode Kofoworola";
+      }
+
+      if(this.loans[index].marketer =='adedoyinojo' ){
+        marketer = "Adedoyin Ojo";
+      }
+
+      if(this.loans[index].marketer =='tugboboayotunde' ){
+        marketer = "Tugbobo Ayotunde";
+      }
+      let json = {
+        "S/No" : index + 1,
+        "Marketer" : marketer,
+        "Start Date" : this.from,
+        "End Date" :  this.to,
+        "Amount." :  this.loans[index].loanamount,
+        "Commission" : 0.02 * this.loans[index].loanamount,
+      }
+
+      
+      this.reportdata[index] = json
+
+    
+    }
+    
+    this.service.exportAsExcelFile(this.reportdata, 'Not Found Upload');
+
   }
 
 }
